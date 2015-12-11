97ef7750918e6bb79e0cf941aaba5a428fcc6f92
davidmillson <davidmillson@users.noreply.github.com>
2015-12-11 16:42:02 +0000

    Update ui.R

--- shiny_example/ui.R
+++ shiny_example/ui.R
@@ -1,29 +1,29 @@
+
 # This is the user-interface definition of a Shiny web application.
 # You can find out more about building applications with Shiny here:
 #
 # http://shiny.rstudio.com
 #
 
-library(shiny)
-
 shinyUI(fluidPage(
-
+  
   # Application title
   titlePanel("Normal Distribution"),
-
+  
   # Sidebar with a slider input for number of bins
   sidebarLayout(
     sidebarPanel(
-      sliderInput("variance",
-                  "Variance of distribution:",
-                  min = 0,
-                  max = 5,
-                  value = 1)
+      sliderInput("min_width",
+                  "Minimum Sepal Width:",
+                  min = 2,
+                  max = 3.5,
+                  step = 0.25,
+                  value = 2.5)
     ),
-
+    
     # Show a plot of the generated distribution
     mainPanel(
-      plotOutput("normalPlot")
+      plotOutput("frequencyPlot")
     )
   )
 ))
